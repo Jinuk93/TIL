6e73430c4d20847cac248c18805f429c5af43733
jinuk93 <rlawlsdnr03@naver.com>
2022-02-20 22:43:00 +0900

    [추가]chap05 배열

---
+++ Java/Hon_Gong_Java/src/chapter05/MainStringArray.java
@@ -0,0 +1,13 @@
+package chapter05;
+
+public class MainStringArray {
+
+	//메인 메소드 호출 후 실행
+	//String[] args에서 [] 비어있는 String타입의 배열에 빈 값을 넣어서 호출한다
+	//args는 변수명으로서, 번지수를 가지고 있다
+	public static void main(String[] args) {
+	
+	System.out.println(args.length);
+
+	}
+}
