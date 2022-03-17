05f294b8cd428653a904edd2c26b63699444062e
jinuk93 <rlawlsdnr03@naver.com>
2022-03-18 01:36:19 +0900

    [추가] Servlet 33_37 docs

--- JSP/2020 Servlet&JSP Programming/Newlecture_JSP_workspace/Newlecture_JSP/src/main/java/com/newlecture/web/Calc3.java
+++ JSP/2020 Servlet&JSP Programming/Newlecture_JSP_workspace/Newlecture_JSP/src/main/java/com/newlecture/web/Calc3.java
@@ -12,8 +12,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 
-@WebServlet("/calc2")
-public class Calc2 extends HttpServlet {
+@WebServlet("/calc3")
+public class Calc3 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -34,8 +34,6 @@ public class Calc2 extends HttpServlet {
 		
 		if(op.equals("=")) {
 			
-			//int x = (Integer)application.getAttribute("value"); //get은 꺼낼것
-			//int x = (Integer)session.getAttribute("value");
 			int x = 0;
 			for(Cookie c : cookies) {
 				if(c.getName().equals("value")) {
@@ -46,8 +44,6 @@ public class Calc2 extends HttpServlet {
 			
 			
 			int y = value;
-			//String operator = (String)application.getAttribute("op");;
-			//String operator = (String)session.getAttribute("op");
 
 			String operator = "";
 			for(Cookie c : cookies) {
@@ -67,14 +63,7 @@ public class Calc2 extends HttpServlet {
 			response.getWriter().printf("result is %d\n", result);
 
 		}else {
-			/*
-			<어플리케이션>
-			application.setAttribute("value", value); //map 컬렉션(key, value)와 같이 저장한다.
-			application.setAttribute("op", op);
-			<세션>
-			session.setAttribute("value", value);  //map 컬렉션(key, value)와 같이 저장한다.
-			session.setAttribute("op", op);*/
-		 
+
 		 // <쿠키 객체 2개생성>
 			Cookie valueCookie = new Cookie("value", String.valueOf(value));
 			Cookie opCookie = new Cookie("op", op);
@@ -82,8 +71,7 @@ public class Calc2 extends HttpServlet {
 			response.addCookie(valueCookie);
 			response.addCookie(opCookie);
 			
-			response.sendRedirect("calc2.html");
-			
+			response.sendRedirect("calc3.html");	
 		}
 	}
 }
