028ea2b873c98716c43b2a128b07d818fa70aad4
Jinuk93 <rlawlsdnr430@gmail.com>
2022-06-11 15:57:57 +0900

    update folder

--- Java/Hon_Gong_Java/src/chapter06_클래스/ch01_03_객체_필드_생성자/Car2.java
+++
@@ -1,9 +0,0 @@
-package chapter06_클래스.ch01_03_객체_필드_생성자;
-
-public class Car2 {
-
-	//생성자
-	Car2(String color, int cc) {
-		
-	}
-}
--- Java/Hon_Gong_Java/src/chapter06_클래스/ch01_03_객체_필드_생성자/CarExample2.java
+++
@@ -1,9 +0,0 @@
-package chapter06_클래스.ch01_03_객체_필드_생성자;
-
-public class CarExample2 {
-
-	public static void main(String[] args) {
-		
-		Car2 myCar2 = new Car2("검정", 3000);
-	}
-}
